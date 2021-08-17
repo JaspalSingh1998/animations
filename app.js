c07be23c657d5f1fff4d35b36476b35ed5e90434
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-17 17:29:05 +0530

    finished navbar toggle

--- app.js
+++ app.js
@@ -34,11 +34,7 @@ function animateSlides() {
       reverse: false,
     })
       .setTween(slideTl)
-      .addIndicators({
-        colorStart: "white",
-        colorTrigger: "white",
-        name: "slide",
-      })
+
       .addTo(controller);
 
     //   New Animation
@@ -54,12 +50,7 @@ function animateSlides() {
     })
       .setPin(slide, { pushFollowers: false })
       .setTween(pageTl)
-      .addIndicators({
-        colorStart: "white",
-        colorTrigger: "white",
-        name: "page",
-        indent: 200,
-      })
+
       .addTo(controller);
   });
 }
@@ -93,10 +84,21 @@ function activeCursor(e) {
 }
 
 function navToggle(e) {
-  e.target.classList.add("active");
-  gsap.to(".line1", 0.5, { rotate: "45", y: "5", background: "black" });
-  gsap.to(".line2", 0.5, { rotate: "-45", y: "-5", background: "black" });
-  gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: "5", background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: "-5", background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: "0", background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: "0", background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
 }
 
 burger.addEventListener("click", navToggle);
